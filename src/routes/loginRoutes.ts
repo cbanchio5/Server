789be836113b8074d781bc0dc084ca7e47fd02a7
cesar banchio <cbanchio5@gmail.com>
2023-02-06 16:26:42 +0100

    added typeguard

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,6 +1,10 @@
 import { Router, Request, Response } from "express";
 import bodyParser from "body-parser";
 
+interface RequestWithBody extends Request {
+  body: {[key:string] : string | undefined}
+}
+
 const router = Router();
 
 router.get('/login', (req: Request, res: Response) => {
@@ -19,10 +23,15 @@ router.get('/login', (req: Request, res: Response) => {
   `)
 })
 
-router.post('/login', (req: Request, res: Response) => {
+router.post('/login', (req: RequestWithBody, res: Response) => {
   const {email, password} = req.body;
 
-  res.send(email + password);
+  if(email) {
+    res.send(email.toUpperCase())
+  } else {
+    res.send('you must provide an email')
+  }
+
 })
 
 export {router}
